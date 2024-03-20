f9be18c268eb3162775794af7db9faf94e7e7602
Koichi Nakata <126118326+Claudioflew@users.noreply.github.com>
2024-03-19 19:16:39 -0700

    Updated

--- copiesElementToPreviousPosition.asm
+++ copiesElementToPreviousPosition.asm
@@ -20,7 +20,7 @@ array dword 1111h, 2222h, 3333h, 4444h, 5555h, 6666h
 .code
 main PROC
 	mov  esi, 0				; ESI points to the first element
-	mov  ecx, lengthof array - 1	; Want to iterate (length - 1) times
+	mov  ecx, lengthof array - 1		; Want to iterate (length - 1) times
 
 L1:	mov  edi, esi				; EDI points to the prev element
 	add  esi, type array			; Updates ESI pointer to the next element
